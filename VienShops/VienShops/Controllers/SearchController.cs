b5bcd4ac9dd2edf73e8be07581cc8e75108deb21
Nguyen The Thoai <thethoainguyenn@gmail.com>
2017-10-19 00:47:08 +0700

    Thoai
    Update Search

---
+++ VienShops/VienShops/Controllers/SearchController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VienShops.Models;
+using PagedList.Mvc;
+using PagedList;
+namespace VienShops.Controllers
+{
+    public class SearchController : Controller
+    {
+        DBVienShopsDataContext Db = new DBVienShopsDataContext();
+        // GET: Search
+        [HttpPost]
+        public ActionResult SearchResults(FormCollection f, int ? page)
+        {
+            // Lấy dc giá trị mà chúng ta tìm kiếm
+            string sKey = f["txtSearch"].ToString();
+            List<SANPHAM> lstResults = Db.SANPHAMs.Where(n => n.TENSP.Contains(sKey)).ToList();
+            // Phân trang
+            int pageSize = 9;
+            int pageNumber = (page ?? 1);
+            if (lstResults.Count == 0)
+            {
+                ViewBag.Notification = "Không tìm thấy sản phẩm nào. Vui lòng thử lại";
+                return RedirectToAction("Index", "Home");
+            }
+            ViewBag.Notification = "Đã tìm thấy " + lstResults.Count() + "kết quả";
+           // ViewBag.ID = id;
+            return View(lstResults.OrderBy(n=>n.TENSP).ToPagedList(pageNumber, pageSize));
+        }
+    }
+}
