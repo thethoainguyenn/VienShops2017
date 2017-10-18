0e0919681bde3cc0d0c2dd0027fc85c04b164c88
Nguyen The Thoai <thethoainguyenn@gmail.com>
2017-10-19 01:15:45 +0700

    Thoai
    Update final search

--- VienShops/VienShops/Controllers/SearchController.cs
+++ VienShops/VienShops/Controllers/SearchController.cs
@@ -13,10 +13,11 @@ namespace VienShops.Controllers
         DBVienShopsDataContext Db = new DBVienShopsDataContext();
         // GET: Search
         [HttpPost]
-        public ActionResult SearchResults(FormCollection f, int ? page)
+        public ActionResult SearchResults(FormCollection f, int? page)
         {
             // Lấy dc giá trị mà chúng ta tìm kiếm
             string sKey = f["txtSearch"].ToString();
+            ViewBag.KeyWord = sKey;
             List<SANPHAM> lstResults = Db.SANPHAMs.Where(n => n.TENSP.Contains(sKey)).ToList();
             // Phân trang
             int pageSize = 9;
@@ -26,9 +27,28 @@ namespace VienShops.Controllers
                 ViewBag.Notification = "Không tìm thấy sản phẩm nào. Vui lòng thử lại";
                 return RedirectToAction("Index", "Home");
             }
+            ViewBag.Notification = "Đã tìm thấy " + lstResults.Count() + " kết quả";
+            // ViewBag.ID = id;
+            return View(lstResults.OrderBy(n => n.TENSP).ToPagedList(pageNumber, pageSize));
+        }
+        [HttpGet]
+        public ActionResult SearchResults(int? page, string sKeyWord)
+        {
+            // Lưu lại tiếp, ViewBag chỉ dc lưu 1 lần,reset lại mất
+            ViewBag.KeyWord = sKeyWord;
+            // Lấy dc giá trị mà chúng ta tìm kiếm
+            List<SANPHAM> lstResults = Db.SANPHAMs.Where(n => n.TENSP.Contains(sKeyWord)).ToList();
+            // Phân trang
+            int pageSize = 9;
+            int pageNumber = (page ?? 1);
+            if (lstResults.Count == 0)
+            {
+                ViewBag.Notification = "Không tìm thấy sản phẩm nào. Vui lòng thử lại";
+                return RedirectToAction("Index", "Home");
+            }
             ViewBag.Notification = "Đã tìm thấy " + lstResults.Count() + "kết quả";
-           // ViewBag.ID = id;
-            return View(lstResults.OrderBy(n=>n.TENSP).ToPagedList(pageNumber, pageSize));
+            // ViewBag.ID = id;
+            return View(lstResults.OrderBy(n => n.TENSP).ToPagedList(pageNumber, pageSize));
         }
     }
 }
