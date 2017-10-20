fadd786933385f9dd683cc1fd9c0c214a245305c
Nguyen The Thoai <thethoainguyenn@gmail.com>
2017-10-20 23:56:48 +0700

    Thoai
    Update Admin Load Product

--- VienShops/VienShops/Controllers/AdminController.cs
+++ VienShops/VienShops/Controllers/AdminController.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using VienShops.Models;
+using PagedList;
+using PagedList.Mvc;
 namespace VienShops.Controllers
 {
     public class AdminController : Controller
     {
+        DBVienShopsDataContext Db = new DBVienShopsDataContext();
         // GET: Admin
-        public ActionResult AdminHome()
+        public ActionResult AdminHome(int? page)
+        {
+            int pageSize = 5;
+            int pageNumber = (page ?? 1);
+            return View(Db.SANPHAMs.ToList().OrderBy(n=>n.TENSP).ToPagedList(pageNumber,pageSize));
+        }
+        // Thêm mới sản phẩm
+        [HttpGet]
+        public ActionResult AddProductNew()
         {
             return View();
         }
